dff79b2c6459cdfb21097096a2f40b889881e7cf
mydearxym <mydearxym@qq.com>
2014-10-21 23:02:26 +0800

    learning day-1

--- TabStylesInspiration/README.md
+++ TabStylesInspiration/README.md
@@ -1,7 +1,22 @@
 ### 各种 tab 切换页的效果
 
 
+#### 学习笔记
+
 > 关于`modernizr`的使用 [请参考这篇文章](http://www.tuicool.com/articles/UVnEVj)
 
+> used css: `letter-spacing`
+
+> [].slice.call 参考[这篇文章](http://stackoverflow.com/questions/2125714/explanation-of-slice-call-in-javascript)
+
+
+```javascript
+ making the function think that it's being called from the parameter (the NodeList returned by document.querySelectorAll('a')) rather than from an array.
+ (String 和 Array都有 slice 方法，但只有Array有forEach)
+ slice 经常用来将 array-like 对象转换为 true array。
+
+```
+
+#### TODO: js 事件冒泡
 
 
--- TabStylesInspiration/js/cbpFWTabs.js
+++ TabStylesInspiration/js/cbpFWTabs.js
@@ -24,8 +24,8 @@
 	function CBPFWTabs( el, options ) {
 		this.el = el;
 		this.options = extend( {}, this.options );
-  		extend( this.options, options );
-  		this._init();
+    extend( this.options, options );
+    this._init();
 	}
 
 	CBPFWTabs.prototype.options = {
@@ -45,16 +45,6 @@
 		this._initEvents();
 	};
 
-	CBPFWTabs.prototype._initEvents = function() {
-		var self = this;
-		this.tabs.forEach( function( tab, idx ) {
-			tab.addEventListener( 'click', function( ev ) {
-				ev.preventDefault();
-				self._show( idx );
-			} );
-		} );
-	};
-
 	CBPFWTabs.prototype._show = function( idx ) {
 		if( this.current >= 0 ) {
 			this.tabs[ this.current ].className = this.items[ this.current ].className = '';
@@ -63,6 +53,16 @@
 		this.current = idx != undefined ? idx : this.options.start >= 0 && this.options.start < this.items.length ? this.options.start : 0;
 		this.tabs[ this.current ].className = 'tab-current';
 		this.items[ this.current ].className = 'content-current';
+	};
+
+	CBPFWTabs.prototype._initEvents = function() {
+		var self = this;
+		this.tabs.forEach( function( tab, idx ) {
+			tab.addEventListener( 'click', function( ev ) {
+				ev.preventDefault();
+				self._show( idx );
+			} );
+		} );
 	};
 
 	// add to global namespace
